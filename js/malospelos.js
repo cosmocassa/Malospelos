e66925e4ae7e1b13ff4b1747dbb362642c757ba8
inaki perez <cosmocassa@gmail.com>
2022-01-30 12:17:37 +0100

    Hasta los botes

--- js/malospelos.js
+++ js/malospelos.js
@@ -1,28 +1,64 @@
+
 document.addEventListener("DOMContentLoaded",()=>{
-    
     const vela = document.getElementById("velaId");
     const velaDiv = document.getElementById("velaIdDiv");
     const estrellita = document.getElementById("estrellita");
-    const boton =document.querySelector(".btn-primary");
-    boton.addEventListener("click",()=>{
-        let i = 255;
-            if (i != 0){
-                setTimeout(oscureceFondo,2000,i);
+    const boton = document.querySelector(".btn-primary");
+    
+    
+
+    boton.addEventListener( "click" ,()=>{
+        var i = 255;
+        var c = 0
+        let brig = 0
+
+        var sumatop = 100;
+        let estrellasize = 300;
+        let estrellaleft = 0;
+        var refreshVela = setInterval(() => {
+
+            if (brig < 1){
+                brig=brig+0.1;
+                vela.style.filter = "brightness("+brig+")";
             }
-            //velaDiv.style.backgroundColor = `rgb(${i}, ${i}, ${i})`;
-            //setTimeout('', 1000);
 
-            //console.log(i);
-            
-            estrellita.style.visibility = "visible"
-            vela.style.backgroundImage = "url(../img/velanoche.png)";
-
-
+            if(estrellaleft>0){
+                estrellaleft=estrellaleft;
+                estrellita.style.marginLeft= estrellaleft + "px"
+            }
+            if(estrellasize < 1900){
+                estrellasize=estrellasize+5;
+                
+                estrellita.style.width = estrellasize + "px"
+            }
         
+            if (c < 100){
+                c++;
+                estrellita.style.opacity = c + "%";
+                if (c<20){
+                    vela.style.filter = "brightness("+(1-(c/100))+")";
+                }
+            }
+        
+            if (sumatop >-140){
+                sumatop = sumatop - 1;
+                estrellita.style.marginTop  =  sumatop + "px";
+            }
+
+            if (i>0){
+                i--;
+                velaDiv.style.backgroundColor = `rgb(${i}, ${i}, ${i})`;
+            }
+            console.log("i:"+i+" c:"+c+" Sumatop:"+sumatop+" Left:"+estrellaleft+" Size:"+estrellasize)
+            if (i === 0 && c===100 && sumatop===-140 && estrellasize === 1900 && estrellaleft === 0){
+                //vela.style.backgroundImage = "url(../img/velanoche.png)";
+                clearInterval(refreshVela);
+                
+            } 
+            
+        
+    }, 0010);
+
+    console.log("fuera");});
     })
-    console.log(vela);
-});
-function oscureceFondo(i){
-    console.log(i);
-    i--;
-}
+
