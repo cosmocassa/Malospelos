b698773ddfd3c2e55d2a21526f6c9ce62159ecf8
inaki perez <cosmocassa@gmail.com>
2022-01-31 13:56:02 +0100

    Hasta lel gato y defer añadido

--- js/malospelos.js
+++ js/malospelos.js
@@ -1,20 +1,116 @@
-
-document.addEventListener("DOMContentLoaded",()=>{
+//document.addEventListener("DOMContentLoaded",()=>{
     const vela = document.getElementById("velaId");
+    const gato = document.getElementById("gatoLuz");
     const velaDiv = document.getElementById("velaIdDiv");
+    const gatoPiedra = document.getElementById("gatoPiedra");
+    const mar = document.getElementById("sea");
     const estrellita = document.getElementById("estrellita");
-    const boton = document.querySelector(".btn-primary");
-    
-    
 
-    boton.addEventListener( "click" ,()=>{
+    var visiVela=false;
+    var visiGato=false;
+    var visiMar=false;
+    var visiGatoPiedra=false;
+
+    window.addEventListener('scroll', function()  {
+        let element = document.getElementById('scroll-content');
+        let element_gatoLuz = document.getElementById('scroll-content-gatoLuz');
+        let element_mar = document.getElementById('scroll-content-mar');
+        let element_gatoPiedra = document.getElementById('scroll-content-gatoPiedra');
+        let screenSize = window.innerHeight;
+        
+        if(element_mar.getBoundingClientRect().top < screenSize) {
+            console.log("Llegamos al mar")
+            if (visiMar===false){
+                animaMar();
+                visiMar=true;
+            }
+        } 
+        
+        if(element.getBoundingClientRect().top < screenSize) {
+            console.log('Llegamos a los botes');
+            if (visiVela===false){
+                animaVela();
+                visiVela=true;
+            }
+        } 
+
+        if(element_gatoLuz.getBoundingClientRect().top < screenSize) {
+            console.log('Llegamos al gato en la escalera');
+            if (visiGato===false){
+                animaGato();
+                visiGato=true;
+            }
+        }
+
+        if(element_gatoPiedra.getBoundingClientRect().top < screenSize) {
+            console.log('Llegamos al gatopiedra');
+            if (visiGatoPiedra===false){
+                animaGatoPiedra();
+                visiGatoPiedra=true;
+            }
+        }
+    });
+
+    function animaMar(){
+        let brig = 0;
+        let subiendo = true;
+        if (visiMar===false){
+            var refreshMar = setInterval(() => {
+                if (brig < 1000) {
+                    if(subiendo){
+                        brig=brig+0.5;
+                    } else {
+                        brig=brig-0.5; 
+                    }
+                    if (brig===0){
+                        brig=brig+0.5;
+                        subiendo=true
+                    }
+                    mar.style.backgroundPositionX = brig+"px";                
+                } else {
+                    if (brig===1000){
+                        brig=brig-0.5;
+                        subiendo=false;
+                    }
+                }
+        
+            })   
+        } 
+    }
+
+    function animaGato(){
+        let brig = 0
+        var refreshGato = setInterval(() => {
+            if (brig < 1){
+                brig=brig+0.001;
+                console.log("sumando")
+                gato.style.filter = "brightness("+brig+")";
+            }
+    
+            if (brig === 0){
+                clearInterval(refreshGato);
+            } 
+        })    
+    }
+    function animaGatoPiedra(){
+        let brig = 0
+        var refreshGatoPiedra = setInterval(() => {
+            if (brig < 1){
+                brig=brig+0.01;
+                gatoPiedra.style.filter = "brightness("+brig+")";
+            }
+    
+            if (brig === 0){
+                clearInterval(refreshGatoPiedra);
+            } 
+        })    
+    }
+    function animaVela(){
         var i = 255;
         var c = 0
         let brig = 0
-
         var sumatop = 100;
         let estrellasize = 300;
-        let estrellaleft = 0;
         var refreshVela = setInterval(() => {
 
             if (brig < 1){
@@ -22,16 +118,11 @@ document.addEventListener("DOMContentLoaded",()=>{
                 vela.style.filter = "brightness("+brig+")";
             }
 
-            if(estrellaleft>0){
-                estrellaleft=estrellaleft;
-                estrellita.style.marginLeft= estrellaleft + "px"
-            }
             if(estrellasize < 1900){
                 estrellasize=estrellasize+5;
-                
                 estrellita.style.width = estrellasize + "px"
             }
-        
+                
             if (c < 100){
                 c++;
                 estrellita.style.opacity = c + "%";
@@ -39,7 +130,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                     vela.style.filter = "brightness("+(1-(c/100))+")";
                 }
             }
-        
+                
             if (sumatop >-140){
                 sumatop = sumatop - 1;
                 estrellita.style.marginTop  =  sumatop + "px";
@@ -49,16 +140,10 @@ document.addEventListener("DOMContentLoaded",()=>{
                 i--;
                 velaDiv.style.backgroundColor = `rgb(${i}, ${i}, ${i})`;
             }
-            console.log("i:"+i+" c:"+c+" Sumatop:"+sumatop+" Left:"+estrellaleft+" Size:"+estrellasize)
-            if (i === 0 && c===100 && sumatop===-140 && estrellasize === 1900 && estrellaleft === 0){
-                //vela.style.backgroundImage = "url(../img/velanoche.png)";
+
+            if (i === 0 && c===100 && sumatop===-140 && estrellasize === 1900){
                 clearInterval(refreshVela);
-                
             } 
-            
-        
-    }, 0010);
-
-    console.log("fuera");});
-    })
-
+        }, 0010);
+    }
+//})
