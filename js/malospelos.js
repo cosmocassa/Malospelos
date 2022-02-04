96575728dc2e5630b17e8426665af0681264e4ef
inaki perez <cosmocassa@gmail.com>
2022-02-04 17:32:54 +0100

    scroll y menu

--- js/malospelos.js
+++ js/malospelos.js
@@ -1,26 +1,54 @@
-//document.addEventListener("DOMContentLoaded",()=>{
-    const vela = document.getElementById("velaId");
-    const gato = document.getElementById("gatoLuz");
-    const velaDiv = document.getElementById("velaIdDiv");
-    const gatoPiedra = document.getElementById("gatoPiedra");
-    const mar = document.getElementById("sea");
-    const estrellita = document.getElementById("estrellita");
+const vela = document.getElementById("velaId");
+const gato = document.getElementById("gatoLuz");
+const velaDiv = document.getElementById("velaIdDiv");
+const gatoPiedra = document.getElementById("gatoPiedra");
+const mar = document.getElementById("sea");
+const estrellita = document.getElementById("estrellita");
 
-    var visiVela=false;
-    var visiGato=false;
-    var visiMar=false;
-    var visiGatoPiedra=false;
+var visiVela=false;
+var visiGato=false; 
+var visiMar=false;
+var visiGatoPiedra=false;
+//Menu
+const body = document.querySelector('body'),
+   
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
 
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+    console.log(sidebar.classList)
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+    }
+});
+
+//MENU FIN
     window.addEventListener('scroll', function()  {
         let element = document.getElementById('scroll-content');
         let element_gatoLuz = document.getElementById('scroll-content-gatoLuz');
         let element_mar = document.getElementById('scroll-content-mar');
         let element_gatoPiedra = document.getElementById('scroll-content-gatoPiedra');
         let screenSize = window.innerHeight;
-        
         if(element_mar.getBoundingClientRect().top < screenSize) {
             console.log("Llegamos al mar")
             if (visiMar===false){
+                //animacion();
                 animaMar();
                 visiMar=true;
             }
@@ -29,6 +57,7 @@
         if(element.getBoundingClientRect().top < screenSize) {
             console.log('Llegamos a los botes');
             if (visiVela===false){
+                //animacion(vela);
                 animaVela();
                 visiVela=true;
             }
@@ -37,7 +66,7 @@
         if(element_gatoLuz.getBoundingClientRect().top < screenSize) {
             console.log('Llegamos al gato en la escalera');
             if (visiGato===false){
-                animaGato();
+                animacion(gato,0);
                 visiGato=true;
             }
         }
@@ -45,105 +74,116 @@
         if(element_gatoPiedra.getBoundingClientRect().top < screenSize) {
             console.log('Llegamos al gatopiedra');
             if (visiGatoPiedra===false){
-                animaGatoPiedra();
+                animacion(gatoPiedra,0);
                 visiGatoPiedra=true;
             }
         }
     });
 
-    function animaMar(){
-        let brig = 0;
-        let subiendo = true;
-        if (visiMar===false){
-            var refreshMar = setInterval(() => {
-                if (brig < 1000) {
-                    if(subiendo){
-                        brig=brig+0.5;
-                    } else {
-                        brig=brig-0.5; 
-                    }
-                    if (brig===0){
-                        brig=brig+0.5;
-                        subiendo=true
-                    }
-                    mar.style.backgroundPositionX = brig+"px";                
-                } else {
-                    if (brig===1000){
-                        brig=brig-0.5;
-                        subiendo=false;
-                    }
-                }
+function animacion(elemento,brigParam){
+    let brig = brigParam;
+    console.log("elemento:"+elemento.id)
+    
+    var refresh = setInterval(() => {
         
-            })   
+        
+        
+       
+        if (brig < 1){
+            brig=brig+0.01;
+            console.log("sumando")
+            elemento.style.filter = "brightness("+brig+")";
+        }
+
+        if (brig === 0){
+            clearInterval(refresh);
         } 
-    }
-
-    function animaGato(){
-        let brig = 0
-        var refreshGato = setInterval(() => {
-            if (brig < 1){
-                brig=brig+0.001;
-                console.log("sumando")
-                gato.style.filter = "brightness("+brig+")";
-            }
+    })    
+}
     
-            if (brig === 0){
-                clearInterval(refreshGato);
-            } 
-        })    
-    }
-    function animaGatoPiedra(){
-        let brig = 0
-        var refreshGatoPiedra = setInterval(() => {
-            if (brig < 1){
-                brig=brig+0.01;
-                gatoPiedra.style.filter = "brightness("+brig+")";
-            }
-    
-            if (brig === 0){
-                clearInterval(refreshGatoPiedra);
-            } 
-        })    
-    }
-    function animaVela(){
-        var i = 255;
-        var c = 0
-        let brig = 0
-        var sumatop = 100;
-        let estrellasize = 300;
-        var refreshVela = setInterval(() => {
-
-            if (brig < 1){
-                brig=brig+0.1;
-                vela.style.filter = "brightness("+brig+")";
-            }
-
-            if(estrellasize < 1900){
-                estrellasize=estrellasize+5;
-                estrellita.style.width = estrellasize + "px"
-            }
-                
-            if (c < 100){
-                c++;
-                estrellita.style.opacity = c + "%";
-                if (c<20){
-                    vela.style.filter = "brightness("+(1-(c/100))+")";
+function animaMar(){
+    let brig = 0;
+    let subiendo = true;
+    if (visiMar===false){
+        var refreshMar = setInterval(() => {
+            mar.style.backgroundPositionX = brig+"px";               
+            if (brig < 1000) {
+                if(subiendo){
+                    brig=brig+0.5
+                } else {
+                   brig=brig-0.5; 
+                }
+                if (brig===0){
+                    brig=brig+0.5;
+                    subiendo=true
+                }
+            } else {
+                if (brig===1000){
+                    brig=brig-0.5;
+                    subiendo=false;
                 }
             }
+        })   
+    } 
+}
+
+function animaVela(){
+    var i = 255;
+    var c = 0
+    let brig = 0
+    var sumatop = 100;
+    let estrellasize = 300;
+    var refreshVela = setInterval(() => {
+        if (brig < 1){
+            brig=brig+0.1;
+            vela.style.filter = "brightness("+brig+")";
+        }
+
+        if(estrellasize < 1900){
+            estrellasize=estrellasize+5;
+            estrellita.style.width = estrellasize + "px"
+        }
                 
-            if (sumatop >-140){
-                sumatop = sumatop - 1;
-                estrellita.style.marginTop  =  sumatop + "px";
+        if (c < 100){
+            c++;
+            estrellita.style.opacity = c + "%";
+            if (c<20){
+                vela.style.filter = "brightness("+(1-(c/100))+")";
             }
+        }   
+                
+        if (sumatop >-140){
+            sumatop = sumatop - 1;
+            estrellita.style.marginTop  =  sumatop + "px";
+        }
 
-            if (i>0){
-                i--;
-                velaDiv.style.backgroundColor = `rgb(${i}, ${i}, ${i})`;
-            }
+        if (i>0){
+            i--;
+            velaDiv.style.backgroundColor = `rgb(${i}, ${i}, ${i})`;
+        }
 
-            if (i === 0 && c===100 && sumatop===-140 && estrellasize === 1900){
-                clearInterval(refreshVela);
-            } 
-        }, 0010);
+        if (i === 0 && c===100 && sumatop===-140 && estrellasize === 1900){
+            clearInterval(refreshVela);
+        } 
+    }, 0010);
+}
+
+function pageScroll(e) {
+    const e2= document.getElementById(e)
+    console.log(e2.getBoundingClientRect().top)
+
+    console.log(e2.getBoundingClientRect.top)
+    let screenSize = window.innerHeight;
+    if (e2.getBoundingClientRect().top + 700 > screenSize){
+        window.scrollBy(0,1); // horizontal and vertical scroll increments
+        scrolldelay = setTimeout(pageScroll,1,e); // scrolls every 100 milliseconds
+    } else{
+        stopScroll();
     }
-//})
+    
+}
+function stopScroll() {
+    clearTimeout(scrolldelay);
+}
+
+
